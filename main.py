d225eda7542223ef10fadd826a06bf1e82775fbf
ThatZacman <zacman0311@hotmail.com>
2023-05-10 00:02:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+import argparse
 import json
 import os
-import sys
 import time
 
-input_file = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", help="path to the input file")
+args = parser.parse_args()
 
-if not os.path.exists(input_file):
-    print(f"No input file found: {input_file}")
-else:
-    with open(input_file, 'r') as f:
-        data = json.load(f)
-        print(f"Processing {input_file}...")
-        print(f"Data: {data}")
-        print("Creating output.txt file...")
-        timestamp = time.strftime("%Y%m%d-%H%M%S")
-        with open(f"output_{timestamp}.txt", "w") as outfile:
-            outfile.write(f"Data: {data}\n")
-        print("Done!")
-        print(f"Output file: output_{timestamp}.txt")
+with open(args.input_file, 'r') as f:
+    data = json.load(f)
+    print(f"Processing {args.input_file}...")
+    print(f"Data: {data}")
+    print("Creating output.txt file...")
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    with open(f"output_{timestamp}.txt", "w") as outfile:
+        outfile.write(f"Data: {data}\n")
+    print("Done!")
