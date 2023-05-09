9dbdc579a9f52686d00dba4f6863de0d47a7a83d
ThatZacman <zacman0311@hotmail.com>
2023-05-09 23:59:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import json
 import os
+import sys
 import time
 
-input_file = os.environ.get('INPUT_FILE')
+input_file = sys.argv[1]
 
 if not os.path.exists(input_file):
-    print(f"No input file found at {input_file}")
+    print(f"No input file found: {input_file}")
 else:
     with open(input_file, 'r') as f:
         data = json.load(f)
@@ -16,3 +17,4 @@ else:
         with open(f"output_{timestamp}.txt", "w") as outfile:
             outfile.write(f"Data: {data}\n")
         print("Done!")
+        print(f"Output file: output_{timestamp}.txt")
