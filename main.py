29cfc0dbe651a7797f419aa2a799754defac14e7
ThatZacman <zacman0311@hotmail.com>
2023-05-09 23:39:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import time
 
 input_file = os.environ.get('INPUT_FILE')
 
-with open(input_file, 'r') as f:
-    data = json.load(f)
-    print(f"Processing {input_file}...")
-    print(f"Data: {data}")
-    print("Creating output.txt file...")
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-    with open(f"output_{timestamp}.txt", "w") as outfile:
-        outfile.write(f"Data: {data}\n")
-    print("Done!")
+if input_file:
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+        print(f"Processing {input_file}...")
+        print(f"Data: {data}")
+        print("Creating output.txt file...")
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        with open(f"output_{timestamp}.txt", "w") as outfile:
+            outfile.write(f"Data: {data}\n")
+        print("Done!")
+else:
+    print("No input file specified!")
