48db74be7232d01963abcbc9c8802eafc4f09733
ThatZacman <zacman0311@hotmail.com>
2023-05-09 23:25:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    input_file = sys.argv[1]
+    input_file = sys.argv[0]
     filename = os.path.basename(input_file)
     print(f"Processing {filename}")
     
